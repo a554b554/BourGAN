708c73a69ca4fe7c846da1b05ef27a0eeb0d420f
a554b554 <chang@cs.columbia.edu>
2018-09-16 15:44:44 -0400

    ..

--- src/tests/module_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    sys.path.append('../')
-    import bourgan
-    from bourgan.gan import BourGAN
-    bourGAN = BourGAN()
-
-
--- src/tests/torch0.4_test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-x = torch.DoubleTensor([1,1,1])
-print(x.type())
-print(torch.cuda.device_count())
-
-
